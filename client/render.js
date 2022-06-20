46e37a4946c06ffb79a2aa60c1bc71f820c1c923
Hiwot Abate <hiwotabate@Hiwots-MacBook-Pro.local>
2022-06-20 19:55:50 -0400

    update on client accepting server response

--- client/render.js
+++ client/render.js
@@ -1,29 +1,41 @@
-const fs = require('fs');
-const { get } = require('http');
 
-let rawdata = fs.readFileSync('../server/serverdata.json');
-let data = JSON.parse(rawdata);
-
-
-function getQuery() {
-    var url_string = window.location.href;
-    var url = new URL(url_string);
-    var usernameParam = url.searchParams.get("q");
-    document.title = usernameParam;
-    return usernameParam;
+function httpGet(url) {
+    let xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.open("GET", url, false);
+    xmlHttpReq.send(null);
+    return xmlHttpReq.responseText;
 }
+var data = "";
 
-var container = document.getElementById("launches-container")
+function renderGrid(url, reverse, containerId, isForUpcoming) {
+    var jsonRawData = httpGet(url);
+    jsonRawData = jsonRawData.slice(0, -1);
+    data = jsonRawData.replace(/(^"|"$)/g, "'");
+    data = JSON.parse(data);
+    console.log(jsonRawData);
+    populateContainer(reverse, containerId, isForUpcoming);
+}
+function populateContainer(reverse, containerId, isForUpcoming) {
+    document.getElementById(containerId).innerHTML = "";
 
-let launchesData = data["results"]
+    var container = document.getElementById(containerId)
 
-for (var launch in launchesData) {
+    let launchesData = data["results"]
+    let currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() - 2);
+    if (reverse) {
+        launchesData.reverse();
+    }
+    for (var launch in launchesData) {
+        let windowStart = Date.parse(launchesData[launch].status.window_start);
+        if (isForUpcoming && Date.parse(currentDate) > windowStart) {
+         continue;
+        }
+        const myJson = JSON.stringify(launchesData[launch]);
 
-    const myJson = JSON.stringify(launchesData[launch]);
+        console.log(myJson);
 
-    console.log(myJson);
-
-    if (myJson.toLowerCase().includes(getQuery().toLowerCase())) {
+        //if (myJson.toLowerCase().includes(getQuery().toLowerCase())) {
         var itemCard = document.createElement("div");
 
 
@@ -31,13 +43,13 @@ for (var launch in launchesData) {
 
         try {
             itemCard.innerHTML = `
-                <img src="${launchesData[launch]["image"]}" alt="delta" class="object-scale-down h-15 w-15">
+                <img src="${launchesData[launch]["image_url"]}" alt="delta" class="object-scale-down h-15 w-15">
                 <div class="m-4">
                     <span class="font-bold">${launchesData[launch]["name"]} <footer></footer></span>
-                    <span class="block font-semibold  text-sm">Rocket Name: ${launchesData[launch]["rocket"]["configuration"]["name"] || "Undefined"}</span>
-                    <span class="block font-semibold  text-sm">Start Date: ${launchesData[launch]["window_start"] || "Undefined"}</span>
-                    <span class="block font-semibold  text-sm">End Date: ${launchesData[launch]["window_end"] || "Undefined"}</span>
-                    <span class="block font-semibold  text-sm">Agency Name: ${launchesData[launch]["launch_service_provider"]["name"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">Rocket Name: ${launchesData[launch]["rocket"]["name"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">Start Date: ${launchesData[launch]["status"]["window_start"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">End Date: ${launchesData[launch]["status"]["window_end"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">Agency Name: ${launchesData[launch]["provider"]["name"] || "Undefined"}</span>
                     <span class="block font-semibold  text-sm">Launchpad Name: ${launchesData[launch]["pad"]["name"] || "Undefined"}</span>
                     <span class="block font-semibold  text-sm">Location: ${launchesData[launch]["pad"]["location"]["name"] || "Undefined"}</span>
                     <span class="block font-semibold  text-sm">Mission Title: ${launchesData[launch]["mission"]["name"] || "Undefined"}</span>
@@ -49,10 +61,10 @@ for (var launch in launchesData) {
                 <img src="${launchesData[launch]["image"]}" alt="delta" class="object-scale-down h-15 w-15">
                 <div class="m-4">
                     <span class="font-bold">${launchesData[launch]["name"]} <footer></footer></span>
-                    <span class="block font-semibold text-sm">Rocket Name: ${launchesData[launch]["rocket"]["configuration"]["name"] || "Undefined"}</span>
-                    <span class="block font-semibold  text-sm">Start Date: ${launchesData[launch]["window_start"] || "Undefined"}</span>
-                    <span class="block font-semibold  text-sm">End Date: ${launchesData[launch]["window_end"] || "Undefined"}</span>
-                    <span class="block font-semibold  text-sm">Agency Name: ${launchesData[launch]["launch_service_provider"]["name"] || "Undefined"}</span>
+                    <span class="block font-semibold text-sm">Rocket Name: ${launchesData[launch]["rocket"]["name"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">Start Date: ${launchesData[launch]["status"]["window_start"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">End Date: ${launchesData[launch]["status"]["window_end"] || "Undefined"}</span>
+                    <span class="block font-semibold  text-sm">Agency Name: ${launchesData[launch]["provider"]["name"] || "Undefined"}</span>
                     <span class="block font-semibold  text-sm">Launchpad Name: ${launchesData[launch]["pad"]["name"] || "Undefined"}</span>
                     <span class="block font-semibold  text-sm">Location: ${launchesData[launch]["pad"]["location"]["name"] || "Undefined"}</span>
                     <span class="block font-semibold  text-sm">Mission Title: Unknown</span>
@@ -61,9 +73,19 @@ for (var launch in launchesData) {
             `
         }
         container.append(itemCard);
-        
     }
+}
+
+
+function getQuery() {
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var usernameParam = url.searchParams.get("q");
+    document.title = usernameParam;
+    return usernameParam;
+}
+
 
     //console.log(launchesData[launch]["name"].toLowerCase(), getQuery().toLowerCase())
 
-}
+
